2f3c8f2564635a267589e62d7645f1708a57f560
Jakob Penninger <jakob.penninger@gmx.net>
2020-08-28 08:27:40 +0200

    Add Main

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        for (int i=1; i<=20; i++) {
+
+            if (i % 3 == 0) {
+                System.out.println("Fizz("+i+")");
+            }
+            else if (i % 5 == 0) {
+                System.out.println("Buzz("+i+")");
+            }
+            else {
+                System.out.println("("+i+")");
+            }
+        }
+    }
+}
